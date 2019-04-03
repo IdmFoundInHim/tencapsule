af4bb539118f16418c8290b3b43476102094c40a
IdmFoundInHim <30935172+IdmFoundInHim@users.noreply.github.com>
2019-04-02 20:27:23 -0400

    Add initial resources

---
+++ auto/pagebuilder.py
@@ -0,0 +1,5 @@
+# ©2019 IdmFoundInHim
+# Ten Capsule Page Builder
+# GOAL: replace all CATCH Comments in base.html with content from input
+#       (which is a specially formatted file) and put the result in a new
+#       document
